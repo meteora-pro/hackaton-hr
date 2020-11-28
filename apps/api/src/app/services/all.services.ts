6371fbc44202880a97bda42128cc523b34b36c13
Filipp Semirov <f.semirov@cm.expert>
2020-11-28 23:55:01 +0300

    fix migrations again!

--- apps/api/src/app/services/all.services.ts
+++ apps/api/src/app/services/all.services.ts
@@ -1,6 +1,5 @@
 import { VacancyService } from './vacancy.service';
 import { CandidateService } from './candidate.service';
-import { SpecializationService } from './specialization.service';
 import { VacancyPredictorService } from './vacancy-predictor.service';
 
-export const allServices = [VacancyService, CandidateService, SpecializationService, VacancyPredictorService];
+export const allServices = [VacancyService, CandidateService, VacancyPredictorService];
