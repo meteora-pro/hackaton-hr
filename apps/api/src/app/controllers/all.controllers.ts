3877e386a46ffc293192cb63257996071ac0ead6
Andrey Maznyak <andreymaznyak@gmail.com>
2020-11-28 23:46:52 +0300

    Remove specialization entity

--- apps/api/src/app/controllers/all.controllers.ts
+++ apps/api/src/app/controllers/all.controllers.ts
@@ -1,11 +1,9 @@
 import { VacancyController } from './vacancy.controller';
 import { CandidateController } from './candidate.controller';
-import { SpecializationController } from './specialization.controller';
 import { VacancyPredictorController } from './vacancy-predictor.controller';
 
 export const allControllers = [
   VacancyController,
   CandidateController,
-  SpecializationController,
   VacancyPredictorController,
 ];
