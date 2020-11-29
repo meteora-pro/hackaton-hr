d2f909cb0565421a1d7d576de77dc811bc7c9e07
s.chernov <s.chernov@cm.expert>
2020-11-29 06:21:03 +0300

    feat: add notification messages to candidateCard state

--- apps/fe/src/app/pages/candidates/candidate-card/candidate-card.actions.ts
+++ apps/fe/src/app/pages/candidates/candidate-card/candidate-card.actions.ts
@@ -8,3 +8,11 @@ export class LoadScoringVacancies {
   public static readonly type = '[CandidateCard] LoadScoringVacancies';
   constructor(public page: number) { }
 }
+
+export class RejectCandidate {
+  public static readonly type = '[CandidateCard] RejectCandidate';
+}
+
+export class ApproveCandidate {
+  public static readonly type = '[CandidateCard] ApproveCandidate';
+}
