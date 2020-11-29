c70c471283371c89644b6ff07a6ffbbc3b7e3278
Filipp Semirov <f.semirov@cm.expert>
2020-11-29 05:14:32 +0300

    implement welcome page

--- apps/fe/src/app/pages/welcome/welcome.component.ts
+++ apps/fe/src/app/pages/welcome/welcome.component.ts
@@ -1,15 +1,56 @@
 import { Component, OnInit } from '@angular/core';
+import { Candidate, Vacancy } from '@meteora/api-interfaces';
+import { Observable } from 'rxjs';
+import { NestCrudService } from '../../shared/services/nest-crud.service';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.less']
+  styleUrls: ['./welcome.component.less'],
 })
 export class WelcomeComponent implements OnInit {
+  constructor(private crudService: NestCrudService<any>) {}
 
-  constructor() { }
+  public candidates$: Observable<Candidate[]>;
+  public vacancies$: Observable<Vacancy[]>;
+  public candidatesLoading = true;
+  public vacanciesLoading = true;
 
   ngOnInit() {
+    this.loadCandidates();
   }
 
+  getPriorityMock(name: string) {
+    switch (true) {
+      case (name || '').length > 50:
+        return 'Высокий';
+      case (name || '').length > 25:
+        return 'Средний';
+      default:
+        return 'Обычный';
+    }
+  }
+
+  getPriorityClass(name: string) {
+    switch (true) {
+      case (name || '').length > 50:
+        return 'priority__primary';
+      case (name || '').length > 25:
+        return 'priority__medium';
+      default:
+        return 'priority__low';
+    }
+  }
+
+  private loadCandidates(): void {
+    this.candidates$ = this.crudService
+      .getEntities('candidate', { limit: 5, sort: {field: 'id', order: 'DESC'} })
+      .pipe(tap(() => (this.candidatesLoading = false))) as Observable<Candidate[]>;
+
+
+    this.vacancies$ = this.crudService
+      .getEntities('vacancy', { limit: 5, sort: {field: 'id', order: 'DESC'} })
+      .pipe(tap(() => (this.vacanciesLoading = false))) as Observable<Vacancy[]>;
+  }
 }
--- apps/fe/src/app/pages/welcome/welcome.module.ts
+++ apps/fe/src/app/pages/welcome/welcome.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 import { WelcomeRoutingModule } from './welcome-routing.module';
 
 import { WelcomeComponent } from './welcome.component';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { SharedModule } from '../../shared/shared.module';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { CommonModule } from '@angular/common';
+import { NzTagModule } from 'ng-zorro-antd/tag';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 
 
 @NgModule({
-  imports: [WelcomeRoutingModule],
+  imports: [WelcomeRoutingModule, NzCardModule, SharedModule, NzTableModule, CommonModule, NzTagModule, NzButtonModule],
   declarations: [WelcomeComponent],
   exports: [WelcomeComponent]
 })
